507745ee70e8fed7abacc58719d07cf33a6eb195
Yusaku Kasahara <yusakukasahara@gmail.com>
2018-01-29 04:39:30 -0500

    server middleware

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
 const waterWalls = require('./functions/waterWalls.js');
 
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', waterWalls)
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'))
+})
+
+app.post('/submit', (req, res) => {
+  console.log('Submitted!');
+});
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!')
