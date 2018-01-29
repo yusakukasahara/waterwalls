9853fbb98ae10f17d9ba404bfffd7cca519f60d5
Yusaku Kasahara <yusakukasahara@gmail.com>
2018-01-29 14:24:26 -0500

    fixed waterwalls logic

--- server/functions/waterWalls.js
+++ server/functions/waterWalls.js
@@ -7,14 +7,18 @@ function waterWalls(array) {
     var left = null;
     for (var j = 0; j < i; j++) {
       if (array[j] > array[i]) {
-        left = j;
+        if (left === null || array[left] < array[j]) {
+          left = j;
+        }
       }
     }
 
     var right = null;
     for (var k = i; k < array.length; k++) {
       if (array[k] > array[i]) {
-        right = k;
+        if (right === null || array[right] < array[k]) {
+          right = k;
+        }
       }
     }
 
@@ -27,9 +31,18 @@ function waterWalls(array) {
   };
 
   for (var l = 0; l < array.length; l++) {
-    for (var m = l + 1; m < array.length; m++) {
-      if (array[m] > array[l]) {
-        var sum = water.slice(l, m).reduce((sum, wall) => sum += wall, 0)
+    for (var m = l + 2; m < array.length; m++) {
+      var valley = [];
+      var valleyArea = [];
+      for (var n = l + 1; n < m; n++) {
+        valley.push(array[n]);
+        valleyArea.push(n);
+      }
+      var maxMiddle = Math.max(...valley)
+      console.log('maxMiddle', maxMiddle)
+      if (maxMiddle < array[m] && maxMiddle < array[l]) {
+        console.log('valleyArea', valleyArea, valleyArea.map(i => water[i]))
+        var sum = valleyArea.map(i => water[i]).reduce((sum, wall) => sum += wall, 0)
         console.log('sum', sum)
         valleys.push([l, m, sum])
       }
@@ -43,9 +56,9 @@ function waterWalls(array) {
   }
   console.log('water: ', water)
   console.log('valleys: ', valleys)
-  return [output[0] + 2, output[1] + 1, output[2]];
+  return {walls: array, water: water, largest: [output[0] + 1, output[1] + 1, output[2]]};
 }
 
-// waterWalls([5, 3, 7, 2, 6, 4, 5, 9, 1, 2])
+// console.log(waterWalls([5, 3, 7, 2, 6, 4, 5, 9, 1, 2]))
 
-module.exports.waterWalls = waterWalls;
+module.exports = waterWalls;
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-// const waterWalls = require('./functions/waterWalls.js');
+const waterWalls = require('./functions/waterWalls.js');
 
 // Body Parser Middleware
 app.use(bodyParser.json());
@@ -20,7 +20,8 @@ app.post('/render', (req, res) => {
                 .map(str => parseInt(str));
   console.log('input', input)
   let output = waterWalls(input);
-  res.json({'result': output})
+  console.log('output', output)
+  res.json(output)
 });
 
 // Server listening at port 3000
@@ -29,50 +30,3 @@ app.listen(3000, () => {
 });
 
 
-function waterWalls(array) {
-  var water = [];
-  var valleys = [];
-  var output = [0, 0, 0];
-
-  for (var i = 0; i < array.length; i++) {
-    var left = null;
-    for (var j = 0; j < i; j++) {
-      if (array[j] > array[i]) {
-        left = j;
-      }
-    }
-
-    var right = null;
-    for (var k = i; k < array.length; k++) {
-      if (array[k] > array[i]) {
-        right = k;
-      }
-    }
-
-    if (right === null || left === null) {
-      water.push(0);
-    } else {
-      water.push(Math.min(array[left], array[right]) - array[i]);
-    }
-
-  };
-
-  for (var l = 0; l < array.length; l++) {
-    for (var m = l + 1; m < array.length; m++) {
-      if (array[m] > array[l]) {
-        var sum = water.slice(l, m).reduce((sum, wall) => sum += wall, 0)
-        console.log('sum', sum)
-        valleys.push([l, m, sum])
-      }
-    }
-  }
-
-  for (var n = 0; n < valleys.length; n++) {
-    if (valleys[n][2] >= output[2]) {
-      output = valleys[n];
-    }
-  }
-  console.log('water: ', water)
-  console.log('valleys: ', valleys)
-  return [output[0] + 2, output[1] + 1, output[2]];
-}
